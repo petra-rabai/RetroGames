939d8859fe814bb7645fb32134cc380e55aeade8
Petra Rábai <petra.rabai@icloud.com>
2022-03-26 12:58:29 +0100

    Add project files.

---
+++ RetroGames/Player.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetroGames
+{
+	public class Player
+	{
+		readonly SecureString securePassword = new SecureString();
+		public string FirstName { get; set; } = "";
+		public string LastName { get; set; } = "";
+		public string Email { get; set; } = "";
+		public string Password { get; set; } = "";
+		public string LoginName { get; set; } = "";
+		public bool IsLoggedIn { get; set; } = false;
+		public bool IsRegistered { get; set; } = false;
+		public bool IsPasswordValid { get; set; } = false;
+		public bool IsPasswordEncrypted { get; set; } = false;
+		public bool IsEmailValid { get; set; } = false;
+		public char PressedKey { get; set; } = ' ';
+		
+		public string GetPlayerFirstName()
+		{
+			string firstName = Console.ReadLine();
+			
+			FirstName = firstName;
+			
+			return FirstName;
+		}
+		public string GetPlayerLastName()
+		{
+			string lastName = Console.ReadLine();
+
+			LastName = lastName;
+
+			return LastName;
+		}
+		public string GetPlayerLoginName()
+		{
+			string loginName = Console.ReadLine();
+
+			LoginName = loginName;
+
+			return LoginName;
+		}
+		public string GetPlayerPassword()
+		{
+			SecureString password = ConvertPasswordToSecure();
+
+			Password = password.ToString();
+
+			return Password;
+		}
+
+		public SecureString ConvertPasswordToSecure()
+		{
+			while (true)
+			{
+				ConsoleKeyInfo hitKeyInfo = Console.ReadKey(true);
+
+				if (hitKeyInfo.Key == ConsoleKey.Enter)
+				{
+					break;
+				}
+				else if (hitKeyInfo.Key == ConsoleKey.Backspace)
+				{
+					if (securePassword.Length > 0)
+					{
+						securePassword.RemoveAt(securePassword.Length - 1);
+						Console.Write("\b \b");
+					}
+				}
+				else if (hitKeyInfo.KeyChar != '\u0000')
+				{
+					securePassword.AppendChar(hitKeyInfo.KeyChar);
+					Console.Write("*");
+				}
+			}
+
+			return securePassword;
+		}
+
+		public string GetPlayerEmail()
+		{
+			string email = Console.ReadLine();
+
+			Email = email;
+
+			return Email;
+		}
+
+		public bool ValidatePassword()
+		{
+			return IsPasswordValid;
+		}
+		public bool ValidateEmail()
+		{
+			return IsEmailValid;
+		}
+		public bool RegistrationSuccess()
+		{
+			return IsRegistered;
+		}
+		public bool LoginSuccess()
+		{
+			return IsLoggedIn;
+		}
+
+	}
+}
